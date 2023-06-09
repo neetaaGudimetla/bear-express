a463d40cf0128d8d7e92134595e9763b4899e738
Your Name <you@example.com>
2023-06-09 11:29:40 +0530

    add new code

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const product = require('./api/product');
 const app = express();
 const PORT = process.env.PORT || 5050;
 app.use("/api/product", product);
+app.get("/products", async (req, res) => {
+    try {
+        res.json({
+            status: 200,
+            message: 'Got Productssss Data successfully',
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Server error");
+    }
+});
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
 /* npm start
